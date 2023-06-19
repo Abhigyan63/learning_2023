f77d7ec5224d6ad1a098926b3b2bb7d31d06daa9
Abhigyan63 <vermaabhigyan4@gmail.com>
2023-06-19 14:07:46 +0530

    commit 4 : main : Added Module1/Day4

---
+++ Module1/Day4/l3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void reverseArray(int arr[], int size) {
+    int i, temp;
+
+    // Swap elements from the outer ends towards the center
+    for (i = 0; i < size / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+}
+
+int main() {
+    int arr[100], size, i;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, size);
+
+    printf("Reversed array: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
